fcdf6b8678c2bb7837e58b9ff3f7d967c4bd7347
kanghana <qazplm12093@naver.com>
2024-05-31 00:17:46 +0900

    fine 더미데이터 생성 완료

---
+++ sql/dummy_study_room_member_fine.sql
@@ -0,0 +1,28 @@
+-- member fine dummy data
+-- 그룹 1 : 1~4 / 그룹2 : 1~6명
+
+-- 그룹 1 dummy data
+INSERT INTO STUDY_ROOM_MEMBER_FINE (study_room_member_id, get_fine_round, fine_amount) VALUES
+(1, 1, 1000),
+(2, 1, 2000),
+(3, 1, 1000),
+(4, 1, 1000),
+(1, 2, 5000),
+(3, 2, 3000)
+;
+
+-- 그룹 2 dummy data
+INSERT INTO STUDY_ROOM_MEMBER_FINE (study_room_member_id, get_fine_round, fine_amount) VALUES
+(1, 1, 3000),
+(2, 1, 1000),
+(5, 1, 6000),
+(1, 2, 4000),
+(2, 2, 3000),
+(4, 2, 2000),
+(6, 2, 5000),
+(2, 3, 6000),
+(3, 3, 1000),
+(4, 3, 8000),
+(5, 3, 2000),
+(6, 3, 5000)
+;
