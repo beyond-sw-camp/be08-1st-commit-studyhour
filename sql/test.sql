cf5595cf1bd5347ed55d8aef1546dad2b163136f
hoyeon96 <hoyen96@naver.com>
2024-05-31 15:50:47 +0900

    1. 테스트케이스 번호 명시 2. #T4 스터디룸 생성 케이스 SQL 구문 추가

--- sql/test.sql
+++ sql/test.sql
@@ -1,15 +1,23 @@
-# T1 
-
-# 신규 유저 등록
+# T1. 신규 유저 등록
 INSERT INTO `USER` (username, password, nickname, gender) VALUES
 ('hoya', '1q2w3e4r!', 'hwsc', 'M');
 
-# 13번 유저 1번 스터디룸 신청
+# T2. 13번 유저 1번 스터디룸 신청
 INSERT INTO study_room_member (user_id, study_room_id, is_join_accepted, join_date_time) VALUES
 (13, 1, 0, '2024-06-01 11:00:20');
 
-# 아이디 hoya 유저(user_id = 13) 신청 가입 허가
+# T3. 아이디 hoya 유저(user_id = 13) 신청 가입 허가
 UPDATE study_room_member
 SET is_join_accepted = 1,
  	 privilege = 'MEMBER'
 WHERE user_id = 13;
+
+
+
+# T4. 스터디룸 생성
+INSERT INTO study_room (study_room_category_id, `name`, is_public, private_password, `description`, max_capacity, start_date_time, end_date_time, created_date_time) VALUES
+(3,'당근마켓 클론 프로젝트',0,'asdf1020','당근마켓 클론 프로젝트 모임입니다.', 6, '2024-06-01 12:12:13', '2024-12-06 05:00:00', '2024-05-31 17:12:31');
+
+-- 스터디룸 생성하면서 동시에 스터디룸 멤버에 방장으로써 데이터 삽입되어야함
+INSERT INTO study_room_member (user_id, study_room_id, is_join_accepted, privilege, join_date_time) VALUES
+(12, 3, 1, 'MANAGER','2024-05-31 17:12:31');
