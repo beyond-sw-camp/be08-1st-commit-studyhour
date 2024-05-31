4a6ff187eeb693458da1316af7ad1ab0573fc88f
kanghana <qazplm12093@naver.com>
2024-05-31 09:33:35 +0900

    벌금 더미데이터 수정

--- sql/dummy_study_room_member_fine.sql
+++ sql/dummy_study_room_member_fine.sql
@@ -3,26 +3,15 @@
 
 -- 그룹 1 dummy data
 INSERT INTO STUDY_ROOM_MEMBER_FINE (study_room_member_id, get_fine_round, fine_amount) VALUES
-(1, 1, 1000),
-(2, 1, 2000),
-(3, 1, 1000),
-(4, 1, 1000),
-(1, 2, 5000),
-(3, 2, 3000)
+(2, 1, 12000),
+(2, 2, 12000),
+(4, 2, 12000)
 ;
 
 -- 그룹 2 dummy data
 INSERT INTO STUDY_ROOM_MEMBER_FINE (study_room_member_id, get_fine_round, fine_amount) VALUES
-(1, 1, 3000),
-(2, 1, 1000),
-(5, 1, 6000),
-(1, 2, 4000),
-(2, 2, 3000),
-(4, 2, 2000),
-(6, 2, 5000),
-(2, 3, 6000),
-(3, 3, 1000),
-(4, 3, 8000),
-(5, 3, 2000),
-(6, 3, 5000)
+(1, 2, 5000),
+(3, 2, 5000),
+(4, 2, 5000),
+(5, 3, 5000)
 ;
