b5c8ed8ee84730aad7ac8c4f5a386ed382c77c5e
hoyeon96 <hoyen96@naver.com>
2024-05-31 15:25:44 +0900

    1. 신규유저 등록 케이스 SQL 구문 2. 신규유저의 스터디룸 가입신청 케이스 SQL 구문 3. 유저의 스터디룸 가입신청 허가시 SQL 구문

---
+++ sql/test.sql
@@ -0,0 +1,15 @@
+# T1 
+
+# 신규 유저 등록
+INSERT INTO `USER` (username, password, nickname, gender) VALUES
+('hoya', '1q2w3e4r!', 'hwsc', 'M');
+
+# 13번 유저 1번 스토리룸 신청
+INSERT INTO study_room_member (user_id, study_room_id, is_join_accepted, join_date_time) VALUES
+(13, 1, 0, '2024-06-01 11:00:20');
+
+# 아이디 hoya 유저(user_id = 13) 신청 가입 허가
+UPDATE study_room_member
+SET is_join_accepted = 1,
+ 	 privilege = 'MEMBER'
+WHERE user_id = 13;
